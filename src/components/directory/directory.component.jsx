639dfad42e2b5393279a653bff91113debbafb32
Henry Kim <henryk805@gmail.com>
2020-12-31 21:57:03 -0800

    shop page, collection preview component added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './directory.styles.scss';
-import MenuItem from '../menu-item/menu-item.component';
+import React from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
 
 // class Directory extends React.Component {
 //     constructor(){
@@ -53,54 +53,51 @@ import MenuItem from '../menu-item/menu-item.component';
 // }
 // export default Directory;
 
-import {useState} from 'react';
+import { useState } from "react";
 
-function Directory (props) {
-    const [sections, setSections] = useState ([
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id:1,
-                    linkUrl: 'shop/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id:2,
-                    linkUrl: 'shop/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id:3,
-                    linkUrl: 'shop/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    id:4,
-                    linkUrl: 'shop/womens',
-                    size: 'large'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    id:5,
-                    linkUrl: 'shop/mens',
-                    size: 'large'
-                }
-            ]);
+function Directory(props) {
+	const [sections, setSections] = useState([
+		{
+			title: "hats",
+			imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+			id: 1,
+			linkUrl: "shop/hats",
+		},
+		{
+			title: "jackets",
+			imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+			id: 2,
+			linkUrl: "shop/jackets",
+		},
+		{
+			title: "sneakers",
+			imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+			id: 3,
+			linkUrl: "shop/sneakers",
+		},
+		{
+			title: "womens",
+			imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+			id: 4,
+			linkUrl: "shop/womens",
+			size: "large",
+		},
+		{
+			title: "mens",
+			imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+			id: 5,
+			linkUrl: "shop/mens",
+			size: "large",
+		},
+	]);
 
-    return (
-        <div className = 'directory-menu'>
-            {
-                sections.map(({id, ...sectionProps}) => (
-                <MenuItem 
-                    key={id} 
-                    {...sectionProps}
-                />))
-            }
-        </div>
-    )};
+	return (
+		<div className='directory-menu'>
+			{sections.map(({ id, ...sectionProps }) => (
+				<MenuItem key={id} {...sectionProps} />
+			))}
+		</div>
+	);
+}
 
 export default Directory;
